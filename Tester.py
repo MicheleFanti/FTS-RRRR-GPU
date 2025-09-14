05b7bc14bfe6a17d58cfe062a08a1f7a0ec9d28b
MicheleFanti <94638941+MicheleFanti@users.noreply.github.com>
2025-09-14 12:24:44 +0200

    Update Tester.py

--- Tester.py
+++ Tester.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
 
     sequences = ["QKLVFFAE"]#, "GVVHGVTTVA", "SLSLLSLSLLS"]
     
-    gridshape = (200, 200, 60)  
+    gridshape = (225, 225, 80)  
     box_lengths = (225, 225) 
     max_iter = 2000  
     dx = 1.5
